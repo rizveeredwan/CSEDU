93ab51f3086a0d3b914e1368fb1b1689b3d5b246
rizveeredwan <rarndc@gmail.com>
2021-07-28 15:44:21 +0600

    added programming lab

---
+++ CSE-110X[Programming]/nfs_pathchola/car/car.h
@@ -0,0 +1,4 @@
+void draw_car(int startx, int starty, int road, int lane, int color);
+void erase_car(int startx, int starty, int road, int lane);
+int lane_formatter(int road, int lane);
+void tilt_car(int croad, int clane, int color);
